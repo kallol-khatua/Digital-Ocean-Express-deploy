eba9dae443da8ae3b7297e9163e18169f0f103b6
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-08-08 17:50:30 +0530

    added env configuration

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -10,7 +12,7 @@ app.get("/api/", (req, res) => {
 })
 
 app.get("/api/getname", async (req, res) => {
-    res.status(200).send({ success: true, message: "Get name", name: "Kallol Khatua" });
+    res.status(200).send({ success: true, message: "Get name", name: process.env.NAME || "Kallol Khatua" });
 })
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")))
